1edfbf1310c22b06f596465e4e2573d131c3ab92
RUTENCO <daniel.ramirez7@udea.edu.co>
2025-09-16 18:01:59 -0500

    parent template that currently calls the panel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Login from '@/app/auth/login/page';
 import Dashboard from '@/app/dashboard/page';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans flex flex-col items-center min-h-screen p-8 sm:p-20 gap-10">
       <Dashboard/>
     </div>
   );
