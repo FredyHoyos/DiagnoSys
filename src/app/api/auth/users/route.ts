2a8ca72de6e7fc66c195934fa029e68af9b63e63
RUTENCO <daniel.ramirez7@udea.edu.co>
2025-09-20 23:56:21 -0500

    users backend

---
+++ src/app/api/auth/users/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users);
+  } catch (error: unknown) {
+    console.error("Error fetching users:", error);
+    let message = "Unknown error";
+    if (error instanceof Error) {
+      message = error.message;
+    }
+    return NextResponse.json(
+      { error: "Error fetching users", details: message },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
