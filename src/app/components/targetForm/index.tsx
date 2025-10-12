035084eb741661eeb73e60db61ddf5e344e25a01
JHON FREDY HOYOS CARDENAS <fredy.hoyos1@udea.edu.co>
2025-10-12 10:07:51 -0500

    feat: add targetForm2

--- src/app/components/targetForm/index.tsx
+++ src/app/components/targetForm/index.tsx
@@ -4,7 +4,7 @@ import { ClipboardList, Grid, Check  } from 'lucide-react';
 const index = ({formsCount=0, categoriesCount=0, itemsCount=0}) => {
   return (
     <div className='flex flex-wrap justify-around gap-4'> 
-        <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center'>
+        <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center hover:shadow-lg hover:scale-105 transition-transform duration-200'>
             <div className="bg-gray-300 rounded-md w-16 h-16 flex items-center justify-center">
                 <ClipboardList size={40} className="text-gray-700" />
                 </div>
@@ -13,7 +13,7 @@ const index = ({formsCount=0, categoriesCount=0, itemsCount=0}) => {
                 <p className='text-gray-600'>Forms</p>
             </div>
             </div>  
-            <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center'>
+            <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center hover:shadow-lg hover:scale-105 transition-transform duration-200'>
             <div className="bg-green-200 rounded-md w-16 h-16 flex items-center justify-center">
                 <Grid size={40} className="text-green-600" />
                 </div>
@@ -22,7 +22,7 @@ const index = ({formsCount=0, categoriesCount=0, itemsCount=0}) => {
                 <p className='text-gray-600'>Categories</p>
             </div>
             </div>  
-            <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center'>
+            <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center hover:shadow-lg hover:scale-105 transition-transform duration-200'>
             <div className="bg-blue-200 rounded-md w-16 h-16 flex items-center justify-center">
                 <Check size={40} className="text-blue-600" />
                 </div>
--- src/app/dashboard/companies/page.tsx
+++ src/app/dashboard/companies/page.tsx
@@ -1,11 +1,13 @@
 import { Target } from 'lucide-react'
 import React from 'react'
 import TargetForm from '@/app/components/targetForm'
+import TargetForm2 from '@/app/components/targetForm2'
 
 const page = () => {
   return (
     <div>
       <TargetForm formsCount={4} categoriesCount={8} itemsCount={15} />
+      <TargetForm2 />
     </div>
   )
 }
