a21f91f9e84c1fb914bc127ec54e4c90a99cf380
JHON FREDY HOYOS CARDENAS <fredy.hoyos1@udea.edu.co>
2025-10-12 09:14:59 -0500

    feat: add targetForm

---
+++ src/app/components/targetForm/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ClipboardList, Grid, Check  } from 'lucide-react';
+
+const index = ({formsCount=0, categoriesCount=0, itemsCount=0}) => {
+  return (
+    <div className='flex flex-wrap justify-around gap-4'> 
+        <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center'>
+            <div className="bg-gray-300 rounded-md w-16 h-16 flex items-center justify-center">
+                <ClipboardList size={40} className="text-gray-700" />
+                </div>
+            <div >
+                <h1 className='text-3xl font-bold'>{formsCount}</h1>
+                <p className='text-gray-600'>Forms</p>
+            </div>
+            </div>  
+            <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center'>
+            <div className="bg-green-200 rounded-md w-16 h-16 flex items-center justify-center">
+                <Grid size={40} className="text-green-600" />
+                </div>
+            <div >
+                <h1 className='text-3xl font-bold'>{categoriesCount}</h1>
+                <p className='text-gray-600'>Categories</p>
+            </div>
+            </div>  
+            <div className='flex flex-row space-x-3 rounded-lg border border-gray-300 p-4 shadow-sm w-48 items-center'>
+            <div className="bg-blue-200 rounded-md w-16 h-16 flex items-center justify-center">
+                <Check size={40} className="text-blue-600" />
+                </div>
+            <div >
+                <h1 className='text-3xl font-bold'>{itemsCount}</h1>
+                <p className='text-gray-600'>Items</p>
+            </div>
+        </div>  
+     </div>
+  )
+}
+
+export default index
--- src/app/dashboard/companies/page.tsx
+++ src/app/dashboard/companies/page.tsx
@@ -1,9 +1,11 @@
+import { Target } from 'lucide-react'
 import React from 'react'
+import TargetForm from '@/app/components/targetForm'
 
 const page = () => {
   return (
     <div>
-      Companies
+      <TargetForm formsCount={4} categoriesCount={8} itemsCount={15} />
     </div>
   )
 }
